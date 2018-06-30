b06485e20cc670bcf3b983730ef81fb38f53f932
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-30 18:59:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,9 +40,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board[index]) == false && index.between?(0, 8)
+  if position_taken?(board, index) == false && index.between?(0, 8)
     return true
-  elsif position_taken?(board[index]) == true
+  elsif position_taken?(board, index) == true
     return false
   else
     return false
