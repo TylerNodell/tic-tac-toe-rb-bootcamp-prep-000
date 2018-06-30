5fa70c5031eeb70e6d179dbeb056018bc5212389
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-30 18:41:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,9 +96,10 @@ end
 
 def turn_count(board)
   counter = 0
-  board.each{|slot| if slot != " "
-    counter += 1
-   }
+  board.each{|slot| 
+    if slot != " "
+      counter += 1
+    end }
   counter
 end
 
